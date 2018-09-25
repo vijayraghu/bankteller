2dd340a04e0a6b049aa8369e8ff81346f61b0788
vijayraghu <vijayasarathypr@gmail.com>
2018-09-25 12:03:09 +0530

    Update testmain.py

--- testmain.py
+++ testmain.py
@@ -16,12 +16,16 @@ from google.cloud import texttospeech_v1beta1 as texttospeech
 
 # Declare global variables
 apiai_client_access_key = os.environ["APIAPI_CLIENT_ACCESS_KEY"]
-apiKey = os.environ["NESSIE_API_KEY"]
+#apiKey = os.environ["NESSIE_API_KEY"]
 apiai_url = "https://api.api.ai/v1/query"
 apiai_querystring = {"v": "20150910"}
 #Setting Google authorization credentials -  Read env data
 credentials_dgf = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
-registered_users = {"+919840610434": "Vijay",
+#Setting Twilio Account Sid and Auth Token
+account_sid = os.environ["account_sid"]
+auth_token = os.environ["auth_token"]
+client = Client(account_sid, auth_token)
+#registered_users = {"+919840610434": "Vijay",
                    "+914444461324": "Vijay"}
 
 # Setup hints for better speech recognition
@@ -29,57 +33,32 @@ hints = "1,2,3,4,5,6,7,8,9,0, 1 one first, 2 two second, 3 three third, 4 four f
 
 app = Flask(__name__)
 
+@app.route('/initialize', methods=['GET','POST'])
+def initialize():
+	call = client.calls.create(
+		to="+14155551212", 
+		from_="+15017122661" 
+		url="/start"
+	)
+	return()
+
 @app.route('/start', methods=['GET','POST'])
 def start():
-	caller_phone_number = request.values.get('From')
-	call_id = request.values.get('CallSid')
+	#caller_phone_number = request.values.get('From')
+	#call_id = request.values.get('CallSid')
 	twilio_asr_language = request.values.get('twilio_asr_language', 'en-IN')
 	apiai_language = request.values.get('apiai_language', 'en')
 	hostname = request.url_root
-	caller_name = registered_users.get(caller_phone_number, ' ')
-    	hostname = request.url_root
+	#caller_name = registered_users.get(caller_phone_number, ' ')
+	output_text = ""
+	
+	resp = VoiceResponse()
 
-    	# Initialize Dialogflow agent
-    	headers = {'authorization': 'Bearer ' + apiai_client_access_key, 
-		   'content-type': 'application/json'
-		  }
-    	payload = {'event': {'name': 'welcome', 
-			     'data': {'user_name': caller_name}
-			    }, 
-		   'lang': apiai_language, 'sessionId': call_id
-		  }
-    
-    	response = requests.request("POST", url=apiai_url, data=json.dumps(payload), headers=headers, params=apiai_querystring)
-    	print response.text
-    	output = json.loads(response.text)
-    	output_text = output['result']['fulfillment']['speech']
-    	output_text = output_text.decode('utf-8')
-    	resp = VoiceResponse()
-
-    	# Prepare for next set of user Speech
+    	# Prepare for user Speech
     	values = {'prior_text': output_text}
     	qs = urllib.urlencode(values)
     	action_url = '/process_speech?' + qs
-    	gather = Gather(input="speech", hints=hints, language=twilio_asr_language, speechTimeout="auto", action=action_url, method="POST")
-    
-	# Welcome prompt played to callers during office hours
-	values = {"text": output_text}
-	qs = urllib.urlencode(values)
-	print 'In start: before Google TTS'
-	gather.play(hostname + 'goog_text2speech?' + qs)
-	print 'In start: after Google TTS'
-	resp.append(gather)
-
-	# If user input is missing after welcome prompt (no speech input), redirect to collect speech input again
-	values = {'prior_text': output_text, 
-		  'twilio_asr_language': twilio_asr_language, 
-		  'apiai_language': apiai_language, 
-		  'SpeechResult': '', 
-		  'Confidence': 0.0
-		 }
-	qs = urllib.urlencode(values)
-	action_url = '/process_speech?' + qs
-	resp.redirect(action_url)
+    	gather = Gather(input="speech", hints=hints, language=twilio_asr_language, speechTimeout="auto", action=action_url, method="POST")  
 	print str(resp)
 	return str(resp)
 
@@ -88,8 +67,8 @@ def start():
 #####
 @app.route('/process_speech', methods=['GET', 'POST'])
 def process_speech():
-	cli = request.values.get('From')
-	call_id = request.values.get('CallSid')
+	#cli = request.values.get('From')
+	#call_id = request.values.get('CallSid')
 	twilio_asr_language = request.values.get('twilio_asr_language', 'en-IN')
 	apiai_language = request.values.get('apiai_language', 'en')
 	prior_text = request.values.get('prior_text', 'Prior text missing')
